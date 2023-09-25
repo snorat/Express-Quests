bce5c92a844d231dc0515c8ab2686baf776acd46
snorat <saimanorat1@gmail.com>
2023-09-25 12:26:27 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favourite user list");
+  console.log("coucou")
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
@@ -21,4 +25,4 @@ app.listen(port, (err) => {
   } else {
     console.log(`Server is listening on ${port}`);
   }
-});
+})
