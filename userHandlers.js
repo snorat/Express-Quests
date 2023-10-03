983d823cf037a0615b699d51c5707b2607a5f2ff
snorat <saimanorat1@gmail.com>
2023-10-03 11:12:55 +0200

    GET en détail, express 06

--- userHandlers.js
+++ userHandlers.js
@@ -57,9 +57,45 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the user");
     });
 };
+// const getUsers = (req, res) => {
+//   database
+//     .query("select * from users")
+//     .then(([users]) => {
+//       res.json(users);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//       res.status(500).send("Error retrieving data from database");
+//     });
+// }
 const getUsers = (req, res) => {
+  const initialSql = "select * from users";
+  const where = [];
+
+  if (req.query.language != null) {
+    where.push({
+      column: "language",
+      value: req.query.language,
+      operator: "=",
+    });
+  }
+  if (req.query.city!= null) {
+    where.push({
+      column: "city",
+      value: req.query.city,
+      operator: "=",
+    });
+  }
+
   database
-    .query("select * from users")
+    .query(
+      where.reduce(
+        (sql, { column, operator }, index) =>
+          `${sql} ${index === 0 ? "where" : "and"} ${column} ${operator} ?`,
+        initialSql
+      ),
+      where.map(({ value }) => value)
+    )
     .then(([users]) => {
       res.json(users);
     })
@@ -67,7 +103,7 @@ const getUsers = (req, res) => {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
     });
-}
+};
 
 const getUsersById = (req, res) => {
   const id = parseInt(req.params.id);
